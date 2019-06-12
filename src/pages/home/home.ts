11ec329ae18721fbd1f85a7ecc925992e53645d3
bc-nogueira <bcnogueira@id.uff.br>
2019-06-11 23:41:38 -0300

    Add timer.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import moment from 'moment';
 
 import { File } from '@ionic-native/file';
 
@@ -12,44 +13,50 @@ declare var window;
 export class HomePage {
   public content: string;
 
-  constructor(public navCtrl: NavController, public file: File) { }
+  constructor(public navCtrl: NavController, public file: File, 
+    public toastController: ToastController) { }
 
   read() {
     var that = this;
-
-    window.resolveLocalFileSystemURL(this.file.externalRootDirectory, function (dirEntry) {
-      that.getFile(dirEntry, "fileToAppend.txt");
+    let beginTimer = moment().valueOf();
+    window.resolveLocalFileSystemURL(this.file.externalRootDirectory + 'Download/', function (dirEntry) {
+      that.getFile(dirEntry, "testIonic.txt", beginTimer);
     }, function(){ 
       console.log('Ocorreu um erro') ;
     });
   }
 
-  getFile(dirEntry, fileName) {
+  getFile(dirEntry, fileName, beginTimer) {
     var that = this;
 
     // Creates a new file or returns the file if it already exists.
     dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
-      that.readFile(fileEntry);
+      that.readFile(fileEntry, beginTimer);
     }, function() {
       console.log('onErrorCreateFile');
     });
 }
 
-  readFile(fileEntry) {
+  readFile(fileEntry, beginTimer) {
     var that = this;
 
     fileEntry.file(function (file) {
-        var reader = new FileReader();
+      var reader = new FileReader();
 
-        reader.onloadend = function() {
-            console.log("Successful file read: " + this.result);
-            that.content = this.result.toString();
-        };
+      reader.onloadend = function() {
+          console.log("Successful file read: " + this.result);
+          that.content = this.result.toString();
+      };
 
-        reader.readAsText(file);
-
+      reader.readAsText(file);
     }, function() {
       console.log('Erro de leitura do arquivo');
     });
+    let endTimer = moment().valueOf();
+      const toast = this.toastController.create({
+        message: 'Finished in ' + (endTimer - beginTimer) + 'ms',
+        duration: 200000
+      });
+      toast.present();
   }
 }
